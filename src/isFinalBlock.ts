d22d5676d58a360427d0c9eb7bf589c91ae4a8ca
Denis Carriere <carriere.denis@gmail.com>
2023-12-30 22:12:27 -0500

    update isFinalBlock

--- src/isFinalBlock.ts
+++ src/isFinalBlock.ts
@@ -4,15 +4,16 @@ import { sleep } from "./utils.js";
 
 const client = new APIClient(Chains.EOS);
 
-export async function get_block_header_state(block_num_or_id: string | number) {
+export async function get_block_info(block_num: number) {
     try {
-        const response = await client.v1.chain.get_block(block_num_or_id);
+        const response = await client.v1.chain.get_block_info(block_num);
         return {
-            block_num: response.block_num.toNumber(),
             block_id: response.id.toString(),
+            block_num: response.block_num.toNumber(),
+            timestamp: Math.floor(response.timestamp.toMilliseconds() / 1000)
         }
-    } catch (e) {
-        // console.error(e)
+    } catch (e: any) {
+        // console.error(e.details)
         return null;
     }
 }
@@ -23,9 +24,9 @@ export async function isFinalBlock(native_block_id: string, native_block_num: nu
 
     // continous attempt to match block_id to block_num
     while (retry > 0) {
-        const header_state = await get_block_header_state(native_block_id);
-        console.log("isFinalBlock", {header_state, native_block_num, retry})
-        if (header_state && header_state.block_num == native_block_num) {
+        const block_info = await get_block_info(native_block_num);
+        console.log("isFinalBlock", {block_info, native_block_num, retry})
+        if (block_info && block_info.block_id == native_block_id) {
             state.finalBlocks.add(native_block_id);
             return true;
         }
@@ -35,4 +36,7 @@ export async function isFinalBlock(native_block_id: string, native_block_num: nu
     return false;
 }
 
-// console.log(await isFinalBlock("14d5b84091c0823e5f70b162a55ea453696255e017b6544014338c7ce55f82db", 349550656));
+// console.log(await isFinalBlock("14d67a33d89def6488c03b87f5c0dda54e5d0e6f4bee0c1090b775c8834446bc", 349600307));
+
+// const block_id = "14d5b84091c0823e5f70b162a55ea453696255e017b6544014338c7ce55f82db";
+// const response = await client.v1.chain.get_block_header_state(349550656);
